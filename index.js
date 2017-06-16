0a0b76ee6a13eadce39ebd96ab3be095f812f325
Fatama Rahman <FatamaRahman@gmail.com>
2017-06-15 20:41:13 -0400

    new design

--- index.js
+++ index.js
@@ -1,70 +1,61 @@
-	function setupTypewriter(t) {
-	    var HTML = t.innerHTML;
+     $('.open-overlay').click(function() {
+       $('.open-overlay').css('pointer-events', 'none');
+       var overlay_navigation = $('.overlay-navigation'),
+         top_bar = $('.bar-top'),
+         middle_bar = $('.bar-middle'),
+         bottom_bar = $('.bar-bottom');
 
-	    t.innerHTML = "";
+       overlay_navigation.toggleClass('overlay-active');
+       if (overlay_navigation.hasClass('overlay-active')) {
 
-	    var cursorPosition = 0,
-	        tag = "",
-	        writingTag = false,
-	        tagOpen = false,
-	        typeSpeed = 100,
-        tempTypeSpeed = 0;
+         top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
+         middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
+         bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
+         overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down')
+         overlay_navigation.velocity('transition.slideLeftIn', {
+           duration: 300,
+           delay: 0,
+           begin: function() {
+             $('nav ul li').velocity('transition.perspectiveLeftIn', {
+               stagger: 150,
+               delay: 0,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [1, 0],
+                 }, {
+                   delay: 10,
+                   duration: 140
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             })
+           }
+         })
 
-	    var type = function() {
-        
-	        if (writingTag === true) {
-	            tag += HTML[cursorPosition];
-	        }
-
-	        if (HTML[cursorPosition] === "<") {
-	            tempTypeSpeed = 0;
-	            if (tagOpen) {
-	                tagOpen = false;
-	                writingTag = true;
-	            } else {
-	                tag = "";
-	                tagOpen = true;
-	                writingTag = true;
-	                tag += HTML[cursorPosition];
-	            }
-	        }
-	        if (!writingTag && tagOpen) {
-	            tag.innerHTML += HTML[cursorPosition];
-	        }
-	        if (!writingTag && !tagOpen) {
-	            if (HTML[cursorPosition] === " ") {
-	                tempTypeSpeed = 0;
-	            }
-	            else {
-	                tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-	            }
-	            t.innerHTML += HTML[cursorPosition];
-	        }
-	        if (writingTag === true && HTML[cursorPosition] === ">") {
-	            tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-	            writingTag = false;
-	            if (tagOpen) {
-	                var newSpan = document.createElement("span");
-	                t.appendChild(newSpan);
-	                newSpan.innerHTML = tag;
-	                tag = newSpan.firstChild;
-	            }
-	        }
-
-	        cursorPosition += 1;
-	        if (cursorPosition < HTML.length - 1) {
-	            setTimeout(type, tempTypeSpeed);
-	        }
-
-	    };
-
-	    return {
-	        type: type
-	    };
-	}
-
-	var typer = document.getElementById('typewriter');
-
-	typewriter = setupTypewriter(typewriter);
-
-	typewriter.type();
+       } else {
+         $('.open-overlay').css('pointer-events', 'none');
+         top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+         middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+         bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+         overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
+         $('nav ul li').velocity('transition.perspectiveRightOut', {
+           stagger: 150,
+           delay: 0,
+           complete: function() {
+             overlay_navigation.velocity('transition.fadeOut', {
+               delay: 0,
+               duration: 300,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [0, 1],
+                 }, {
+                   delay: 0,
+                   duration: 50
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             });
+           }
+         })
+       }
+     })
